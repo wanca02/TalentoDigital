c31e4aa69022ef9e2a5bf531384ec2f8f6b38ee3
Waldo Ancacoy <34361167+wanca02@users.noreply.github.com>
2020-01-31 07:40:47 -0600

    Up Jasmine files
    Para ejecutar use: npm test
    edite package.json para ejecutar nuevos archivos

---
+++ Jasmin_folder/spec/hwSpec.js
@@ -0,0 +1,42 @@
+//Devuelve la suma de 1 a N. Por ejemplo,  Sum1toN(3) devolverá la suma de 1+2+3, la cual es 6
+//sum1toN(255) devuelve la suma de todos los números de 1 a 255
+function Sum1toN(n){
+  var suma=0;
+  for (var i = 1; i <= n; i++) {
+    suma += i;    
+  }
+  return suma;
+}
+//devuelve la suma del primer y último número en el array
+function sumFirstLast(arr){
+  return arr[0]+arr[arr.length-1]
+}
+var n=2;
+console.log("Suma 1 to",n,":",Sum1toN(n));
+
+var a=[1,2,3,4];
+console.log("Suma",a[0],"+",a[a.length-1],"=",sumFirstLast(a));
+
+describe("Sum1toN", function() { 
+  it("should return 3 when we pass 2 as an argument", function() { 
+    expect(Sum1toN(2)).toEqual(3); 
+  }); 
+  it("should return 6 when we pass 3 as an argument", function() { 
+    expect(Sum1toN(3)).toEqual(6); 
+  }); 
+  it("should return 10 when we pass 4 as an argument", function() { 
+    expect(Sum1toN(4)).toEqual(10); 
+  }); 
+});
+    
+describe("sumFirstLast", function() { 
+  it("should return 3 when we pass [1,2] as an argument", function() { 
+    expect(sumFirstLast([1,2])).toEqual(3); 
+  }); 
+  it("should return 10 when we pass [2,3,8] as an argument", function() { 
+    expect(sumFirstLast([2,3,8])).toEqual(10); 
+  }); 
+  it("should return -10 when we pass [-6,23,3,-4] as an argument", function() { 
+    expect(sumFirstLast([-6,23,3,-4])).toEqual(-10); 
+  }); 
+});
