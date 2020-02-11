d6aa36f8720c653eb83f845614012c841afd4c09
wanca02 <wancacoy2014@alu.uct.cl>
2020-02-11 08:13:40 -0600

    js fx fast

--- 4.API-AJAX/pokemon/js/script.js
+++ 4.API-AJAX/pokemon/js/script.js
@@ -2,16 +2,21 @@ $(document).ready(Main);
 
 function Main() {
   for (let i = 1; i <= 150; i++) {
-    //addImg("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+i+".png");
-    addImgtxt("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+i+".png");
+    addImg("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+i+".png");
+    //addImgtxt("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+i+".png");
+  }
 }
 
+// CON ESTE ES MAS RAPIDO :3
 function addImg(i) {
   var img=document.createElement("img");
   img.setAttribute("src",i);
   $(".container").append(img);
 }
+//eSTE ES MAS LENTO
+/*
 function addImgtxt(i) {
   var txt=$("<img>").attr("src",i);
   $(".container").append(txt);
 }
+*/
