1ef4df6dd2e3d69446b80768fb50b0b2a68ee819
wanca02 <wancacoy2014@alu.uct.cl>
2020-06-24 13:59:28 -0400

    del pets

--- 6.JAVA/javaOnWeb/mascotas/src/com/pets/web/models/Cat.java
+++
@@ -1,9 +0,0 @@
-package com.pets.web.models;
-
-public class Cat extends Animal{
-	
-	public Animal createPet(String name, String breed, int weight) {
-		Animal animal= new Animal(name,breed,weight);
-		return animal;
-	}
-}
--- 6.JAVA/javaOnWeb/mascotas/src/com/pets/web/models/Pet.java
+++
@@ -1,7 +0,0 @@
-package com.pets.web.models;
-
-public interface Pet {
-	public static void showAffection() {
-		System.out.println("Se creó la mascota...");
-	}
-}
